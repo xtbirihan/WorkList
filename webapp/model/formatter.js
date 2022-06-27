0c619a95f2fa6e86c43f9b99a5cee5afb8332b97
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-27 14:05:00 +0000

    Sample: Worklist App - Step 3 - Extending the Worklist Table

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,13 +1,34 @@
-sap.ui.define([], function () {
+sap.ui.define([
+	"sap/ui/core/library"
+] , function (coreLibrary) {
 	"use strict";
+
+	// shortcut for sap.ui.core.ValueState
+	var ValueState = coreLibrary.ValueState;
 
 	return {
 
+		/**
+		 * Rounds the number unit value to 2 digits
+		 * @public
+		 * @param {string} sValue the number string to be rounded
+		 * @returns {string} sValue with 2 digits rounded
+		 */
 		numberUnit : function (sValue) {
 			if (!sValue) {
 				return "";
 			}
 			return parseFloat(sValue).toFixed(2);
+		},
+
+		quantityState: function(iValue) {
+			if (iValue === 0) {
+				return ValueState.Error;
+			} else if (iValue <= 10) {
+				return ValueState.Warning;
+			} else {
+				return ValueState.Success;
+			}
 		}
 
 	};
